6df3c0ac13b5a5a2e88b08aba942fd9bb30c7727
PANGTO APHISIT <61449550+muzakal023@users.noreply.github.com>
2020-08-28 07:58:28 +0900

    Initial commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="footer-content container">
+        <!-- <div class="made-with">Made with <i class="fa fa-heart heart"></i> by Andre Madarang</div>
+        -->      
+        {{ menu('footer', 'partials.menus.footer') }}
+    </div> <!-- end footer-content -->
+</footer>
